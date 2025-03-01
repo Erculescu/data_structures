6d4c1a064e83e44a50ca5cf85cd50c0e606d8033
Erculescu Rares <erculescur@gmail.com>
2025-03-01 22:55:55 +0200

    #1 Seminar debugger 101

---
+++ Seminar1/Seminar1/Program.cpp
@@ -0,0 +1,15 @@
+
+
+
+int main() {
+
+	char x = 65;
+	/*char si int sunt by default signed*/
+	/*signed stocheaza un bit de semn, cel din stanga*/
+	/*65 in binar e 01000001*/
+	/*65 in hexa e 0x41*/
+	char y = -2;
+	float z = 65.64;
+
+	return 0;
+}
